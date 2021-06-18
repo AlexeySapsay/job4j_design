1841c7cc2ebfd0f2173c8a9122fda9123d926e90
AlekseySapsay <pheniksesalex@mail.ru>
2021-06-18 20:09:22 +0300

    1. Что такое итератор. [#4951]

--- src/test/java/ru/job4j/it/BackwardArrayItTest.java
+++ src/test/java/ru/job4j/it/BackwardArrayItTest.java
@@ -3,24 +3,24 @@ package ru.job4j.it;
 import org.junit.Test;
 import java.util.NoSuchElementException;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class BackwardArrayItTest {
     @Test
     public void whenMultiCallhasNextThenTrue() {
         BackwardArrayIt it = new BackwardArrayIt(new int[]{1, 2, 3, 4});
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
-        assertThat(it.hasNext(), is(true));
+        assertEquals(it.hasNext(), is(true));
+        assertEquals(it.hasNext(), is(true));
+        assertEquals(it.hasNext(), is(true));
     }
 
     @Test
     public void whenReadSequence() {
         BackwardArrayIt it = new BackwardArrayIt(new int[]{1, 2, 3, 4});
-        assertThat(it.next(), is(4));
-        assertThat(it.next(), is(3));
-        assertThat(it.next(), is(2));
-        assertThat(it.next(), is(1));
+        assertEquals(it.next(), is(4));
+        assertEquals(it.next(), is(3));
+        assertEquals(it.next(), is(2));
+        assertEquals(it.next(), is(1));
     }
 
     @Test(expected = NoSuchElementException.class)
