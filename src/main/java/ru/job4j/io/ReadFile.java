1e0f4426f9195b6c70868549138d015f0a3bc9b6
AlekseySapsay <pheniksesalex@mail.ru>
2021-08-31 11:15:08 +0300

    0.3. BufferedReader. [#252489 #196444]

--- src/main/java/ru/job4j/io/ReadFile.java
+++ src/main/java/ru/job4j/io/ReadFile.java
@@ -1,43 +1,24 @@
 package ru.job4j.io;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
 
 /**
  * Программа выполняет чтение данных из файла
  * и выводит их на консоль
- *
+ * задача по ссылке: https://job4j.ru/profile/exercise/45/task-view/307
  * @author AlexSapsay (sapsayalexey@gmail.com)
  * @version 1.0
  * @since 29.08.2021
  */
 
-//https://job4j.ru/profile/exercise/45/task-view/307
 public class ReadFile {
     public static void main(String[] args) {
-        // читаем по байтово файл
-        //        try (FileInputStream in = new FileInputStream("input.txt")) {
-//            StringBuilder text = new StringBuilder();
-//            int read;
-//            while ((read = in.read()) != -1) {
-//                text.append((char) read);
-//            }
-//            String[] lines = text.toString().split(System.lineSeparator());
-//            for (String line : lines) {
-//                System.out.println(line);
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
         // читаем файл в буфер
-
         try (BufferedReader in = new BufferedReader(new FileReader("input.txt"))) {
             in.lines().forEach(System.out::println);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
-
 }
