156032d5cae771be840fb159caeb1525dabed3ae
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-03 00:27:06 +0300

    3.0. Тестирование IO [#173905]

---
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,93 @@
+package ru.job4j.io;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+import static org.hamcrest.Matchers.is;
+
+
+import java.io.*;
+
+import static org.junit.Assert.*;
+
+public class AnalizyTest {
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void testServerLogsUnavailable1() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("200 10:59:01");
+            out.println("500 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        // формально вывод правильный, только как разделить даты
+        // с помощью System.lineSeparator() в данном случае не представляю????
+        //"10:57:01;10:59:01System.lineSeparator()11:01:02;11:02:02"
+        assertThat(rsl.toString(), is("10:57:01;10:59:0111:01:02;11:02:02"));
+    }
+
+    @Test
+    public void testServerLogsUnavailable2() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+
+        assertThat(rsl.toString(), is("10:57:01;11:02:02"));
+    }
+
+    @Test
+    public void testServerLogsUnavailable3() throws IOException {
+        File source = folder.newFile("source.txt");
+        File target = folder.newFile("target.txt");
+        try (PrintWriter out = new PrintWriter(source)) {
+            out.println("200 10:56:01");
+            out.println("500 10:57:01");
+            out.println("400 10:58:01");
+            out.println("500 10:59:01");
+            out.println("400 11:01:02");
+            out.println("200 11:02:02");
+            out.println("200 11:03:02");
+            out.println("400 11:05:02");
+            out.println("400 11:06:02");
+            out.println("500 11:07:02");
+            out.println("400 11:08:02");
+            out.println("100 11:09:20");
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        StringBuilder rsl = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new FileReader(target))) {
+            in.lines().forEach(rsl::append);
+        }
+        // формально вывод правильный, только как разделить даты
+        // с помощью System.lineSeparator() в данном случае не представляю
+        //"10:57:01;11:02:02System.lineSeparator()11:05:02;11:09:20"?????????
+        assertThat(rsl.toString(), is("10:57:01;11:02:0211:05:02;11:09:20"));
+    }
+}
