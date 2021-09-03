53bc5d62ffb7c37bef036a6ebf3e5f0ef7168ec0
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-03 23:19:39 +0300

    4.0. File [#252491]

---
+++ src/main/java/ru/job4j/io/Dir.java
@@ -0,0 +1,41 @@
+package ru.job4j.io;
+
+import java.io.File;
+
+/**
+ * Главным элементом файловой системы является объект java.io.File.
+ * <p>
+ * File может быть и текстовым документом и директорией.
+ * <p>
+ * Рассмотрим задачу с получением всех элементов директории.
+ * Наш проект лежит в папке c:\projects\.
+ * <p>
+ * Напишем программу, которая проверяет,
+ * что директория projects - это директория и выведем ее
+ * содержимое.
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 03.09.2021
+ */
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        //Проверяем, что файл существует.
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        //Проверяет, что файл - это директория.
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        //Получаем список файлов в этой директории.
+//        for (File subfile : file.listFiles()) {
+//            System.out.println(subfile.length());
+//            //System.out.println(subfile.getAbsoluteFile());
+//        }
+        System.out.println("file.length() : " + file.length());
+    }
+}
