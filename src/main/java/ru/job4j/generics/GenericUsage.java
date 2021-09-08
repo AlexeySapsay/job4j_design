fff5ecaf1de208f0dd18edee84f5e49205fa8d03
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-08 12:13:01 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/generics/GenericUsage.java
+++ src/main/java/ru/job4j/generics/GenericUsage.java
@@ -2,48 +2,17 @@ package ru.job4j.generics;
 
 import java.util.*;
 
-import ru.job4j.generics.Person;
-
 public class GenericUsage {
     public static void main(String[] args) {
-        //List<String> list = new ArrayList<>();
-        List list = new ArrayList();
+        List<String> list = new ArrayList<>();
         list.add("first");
         list.add("second");
         list.add("third");
-        //list.add(new Person("Alex", 32, new Date(913716000000L)), 1);
-        list.add(new Person("name", 21, new Date(913716000000L)));
-        //String s1 = list.get(1);
-//        for (String s : list) {
-//            System.out.println("Текущий элемент: " + s);
-//        }
-//        for (int i = 0; i < list.size(); i++) {
-//            String s = (String) list.get(i);
-//            System.out.println("Current element: " + s);
-//       }
-
-//        List<Integer> l = List.of(1, 2, 3, 4, 5);
-//        new GenericUsage().printRsl(l);
-//
-//        List<Person> per = List.of(new Person("name", 21, new Date(913716000000L)));
-//        new GenericUsage().printRsl(per);
-//
-//        List<Programmer> pro = List.of(new Programmer("name123", 23, new Date(913716000000L)));
-//        //new GenericUsage().printRsl(pro);
-//        new GenericUsage().printInfo(pro);
-
+        //list.add(new Person("name", 21, new Date(913716000000L)));
         List<? super Integer> list1 = new ArrayList<>();
         new GenericUsage().addAll(list1);
     }
 
-    //    public void printRsl(Collection<Object> col) {
-//        for (Iterator<Object> it = col.iterator();
-//             it.hasNext(); ) {
-//            Object next = it.next();
-//            System.out.println(next);
-//        }
-//    }
-    @SuppressWarnings("checkstyle:EmptyForIteratorPad")
     public void printRsl(Collection<?> col) {
         for (Iterator<?> it = col.iterator();
                 it.hasNext();) {
--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,6 +9,7 @@ import java.util.function.Predicate;
 
 /**
  * https://job4j.ru/profile/exercise/45/task-view/314
+ * https://job4j.ru/profile/exercise/45/task-view/316
  * <p>
  * Разработайте программу Search, которая будет искать
  * файлы только по определенному предикату.
@@ -20,11 +21,22 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile()
+        // валидация входных параметров
+        if (args.length == 0 || args.length == 1) {
+            throw new IllegalArgumentException(
+                    "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER."
+                            + System.lineSeparator()
+                            + "Enter path to dir search and extension of file like:"
+                            + System.lineSeparator()
+                            + "/some/path extension");
+        }
+        // чтобы начальная папка передавалась через аргументы запуска.
+        Path start = Paths.get(args[0]);
+        String word = args[1];
+
+        System.out.println(search(start, p -> p.toFile()
                 .getName()
-                .endsWith("js"))
-                .forEach(System.out::println);
+                .endsWith(word)));
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition)
