4aeab7deaa52b565852aa4d0b2b3e569e0f1eddf
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-09 23:35:35 +0300

    5.1. Именованные аргументы [#295518 #200912]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -22,29 +22,20 @@ public class ArgsName {
      *
      * @param key ключь для извлечения значения value из hashMap
      * @return value, возвращаемоое значение из хэшмапы
-     *
      */
     public String get(String key) {
         return values.get(key);
     }
 
     private void parse(String[] args) {
-        // проводим валидацию входных именнованных аргументов
         validation(args);
 
-        // парсим пару, сплитя по знаку равно получаем ключ значение
         for (String str : args) {
-            String key = "";
-            String value = "";
-
             String[] keyAndValue = str.split("=");
-            key = keyAndValue[0];
-            value = keyAndValue[1];
-//            System.out.println("key: " + key + System.lineSeparator()
-//                    + "value :" + value);
-            // чистим ключ от первого тире -
-            // и заносим пару ключь = значение в мапу
-            key = key.substring(1, key.length());
+            String key = keyAndValue[0];
+            String value = keyAndValue[1];
+
+            key = key.substring(1);
             values.put(key, value);
         }
     }
@@ -62,7 +53,7 @@ public class ArgsName {
      * @param args параметры, полученные из терминала или командной строки
      */
     public static void validation(String[] args) {
-        if (args.length == 0) {
+        if (args.length != 2) {
             throw new IllegalArgumentException(" Arguments is not correct:"
                     + System.lineSeparator()
                     + "Arguments is null"
@@ -73,10 +64,7 @@ public class ArgsName {
                 throw new IllegalArgumentException(" Arguments is not correct:"
                         + "Use argument like this: -Xmx=514");
             }
-            // парсинг стринга
-            // стрингу разбиваю на ключь и значение и проверяю
-            // отличие от нуля длины ключа и значения
-            // и наличие =
+
             String[] strings;
             strings = str.split("=");
             System.out.println("strings.length : " + strings.length);
@@ -91,13 +79,5 @@ public class ArgsName {
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
-        System.out.println(jvm.get("Xmx"));
-
-        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
-        System.out.println(zip.get("out"));
-
-        ArgsName pip = ArgsName.of(new String[]{"-out=", "-encoding=UTF-8"});
-        System.out.println(pip.get("out"));
     }
 }
