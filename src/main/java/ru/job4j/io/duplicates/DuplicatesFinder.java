c85614350e38cf47bb6e8cd0e90cd8e59032d26d
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-11 12:06:16 +0300

    4.2. Поиск дубликатов [#315066 #201189] удалили лишние комментарии. Вынес валидацию именнованных аргументов в отдельный метод. Добавил проверку существования директории.

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -2,6 +2,7 @@ package ru.job4j.io.duplicates;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.NotDirectoryException;
 import java.nio.file.Path;
 
 /**
@@ -19,14 +20,26 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        if (args.length == 0) {
+        DuplicatesFinder duplicatesFinder = new DuplicatesFinder();
+        duplicatesFinder.validate(args);
+
+        Path start = Path.of(args[0]);
+        Files.walkFileTree(start, new DuplicatesVisitor());
+    }
+
+    private void validate(String[] args) {
+        if (args.length != 1) {
             throw new IllegalArgumentException(
                     "You have a mistake in line:"
                             + System.lineSeparator()
                             + "Use : java -jar duplicatesFinder.jar directoryName_for_scan");
         }
-
-        Path start = Path.of(args[0]);
-        Files.walkFileTree(start, new DuplicatesVisitor());
+        try {
+            if (!Files.exists(Path.of(args[0]))) {
+                throw new Exception("Directory not found: ");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
