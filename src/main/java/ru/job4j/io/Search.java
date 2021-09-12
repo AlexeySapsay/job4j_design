e4d4ab7713d12aa882e0fafa9007c016c85541fe
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-13 00:36:16 +0300

    5. Валидация параметров запуска. [#246865 #200531] рефакторинг согластно замечаниям ментора

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -21,20 +21,30 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
+        Search searchInstance = new Search();
         // валидация входных параметров
-        validation(args);
+        searchInstance.validation(args);
 
         // Начальная папка передавалась через аргументы запуска,
         // указываемыми параметроами в  терминале или командной строке.
         Path start = Paths.get(args[0]);
         String word = args[1];
 
-        System.out.println(search(start, p -> p.toFile()
+        System.out.println(searchInstance.search(start, p -> p.toFile()
                 .getName()
                 .endsWith(word)));
     }
 
-    public static List<Path> search(Path root, Predicate<Path> condition)
+    /**
+     * Метод рекурсивно обходит дирректорию и добавляет в List
+     * файлы, удовлятворяющие условияю condition
+     *
+     * @param root      дирректория с которой начнется поиск
+     * @param condition условие для поиска файлов
+     * @return List путей до найденных файлов, удовлетворяющих condition
+     * @throws IOException выкидывается в случае, когда все плохо.
+     */
+    public List<Path> search(Path root, Predicate<Path> condition)
             throws IOException {
         SearchFiles searcher = new SearchFiles(condition);
         Files.walkFileTree(root, searcher);
@@ -49,12 +59,11 @@ public class Search {
      *                argsArr[0] имя папки с которой начать поиск
      *                argsArr[1] расширение файлов, которые будем искать
      */
-
-    public static void validation(String[] argsArr) {
+    public void validation(String[] argsArr) {
         Path dir = Paths.get(String.valueOf(argsArr[0]));
-        if (argsArr[0] == null
+        if (argsArr.length != 2
+                || argsArr[0] == null
                 || argsArr[1] == null
-                || argsArr.length != 2
                 || !Files.exists(dir)) {
             throw new IllegalArgumentException(
                     "Root folder is null. Usage java -jar dir.jar ROOT_FOLDER."
