5b79405b0dc08f4fb7fd108b7b109325011c32e1
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-13 18:05:05 +0300

    5.1. Именованные аргументы [#295518 #200912] удалил закомменченный код и причисал, дописал джаваДок над методами.

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -28,6 +28,10 @@ public class ArgsName {
         return values.get(key);
     }
 
+    /**
+     * Метод получения множества ключей
+     * @return множество ключей из values
+     */
     public Set<String> getKeys() {
         return values.keySet();
     }
@@ -59,34 +63,6 @@ public class ArgsName {
         return names;
 
     }
-
-
-//    public void validation(String[] args) {
-//        if (args.length != 2) {
-//            throw new IllegalArgumentException(" Arguments is not correct:"
-//                    + System.lineSeparator()
-//                    + "Arguments is null"
-//                    + "Use argument like this: -Xmx=514");
-//        }
-//        for (String str : args) {
-//            if (str.length() == 0 || !str.contains("=")) {
-//                throw new IllegalArgumentException(" Arguments is not correct:"
-//                        + "Use argument like this: -Xmx=514");
-//            }
-//
-//            String[] strings;
-//            strings = str.split("=");
-//            if (strings.length != 2) {
-//                throw new IllegalArgumentException(
-//                        System.lineSeparator()
-//                                + "That form arguments is not correct: -Xmx= OR =514"
-//                                + System.lineSeparator()
-//                                + "Use argument like this: -Xmx=514");
-//            }
-//        }
-//    }
-
     public static void main(String[] args) {
-
     }
 }
