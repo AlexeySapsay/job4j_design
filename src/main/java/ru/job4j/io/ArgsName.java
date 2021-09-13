6e49c2490d18162c72f68cc03e4ccaa074660c2c
AlekseySapsay <pheniksesalex@mail.ru>
2021-09-13 18:02:54 +0300

    5.1. Именованные аргументы [#295518 #200912] удалил метод validate() заменил его на внутреннюю валидацию в parse()

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -32,16 +32,24 @@ public class ArgsName {
         return values.keySet();
     }
 
+    /**
+     * Метод валидации входных параметров,
+     * заданных через терминал или командную строку
+     *
+     * @param args параметры, полученные из терминала или командной строки
+     */
     private void parse(String[] args) {
-        validation(args);
-
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Use correct argument like key=value");
+        }
         for (String str : args) {
             String[] keyAndValue = str.split("=");
+            if (keyAndValue.length < 2) {
+                throw new IllegalArgumentException("Use like key=value");
+            }
             String key = keyAndValue[0];
             String value = keyAndValue[1];
-
-            key = key.substring(1);
-            values.put(key, value);
+            values.put(key.substring(1), value);
         }
     }
 
@@ -52,36 +60,31 @@ public class ArgsName {
 
     }
 
-    /**
-     * Метод валидации входных параметров,
-     * заданных через терминал или командную строку
-     *
-     * @param args параметры, полученные из терминала или командной строки
-     */
-    public void validation(String[] args) {
-        if (args.length != 2) {
-            throw new IllegalArgumentException(" Arguments is not correct:"
-                    + System.lineSeparator()
-                    + "Arguments is null"
-                    + "Use argument like this: -Xmx=514");
-        }
-        for (String str : args) {
-            if (str.length() == 0 || !str.contains("=")) {
-                throw new IllegalArgumentException(" Arguments is not correct:"
-                        + "Use argument like this: -Xmx=514");
-            }
 
-            String[] strings;
-            strings = str.split("=");
-            if (strings.length != 2) {
-                throw new IllegalArgumentException(
-                        System.lineSeparator()
-                                + "That form arguments is not correct: -Xmx= OR =514"
-                                + System.lineSeparator()
-                                + "Use argument like this: -Xmx=514");
-            }
-        }
-    }
+//    public void validation(String[] args) {
+//        if (args.length != 2) {
+//            throw new IllegalArgumentException(" Arguments is not correct:"
+//                    + System.lineSeparator()
+//                    + "Arguments is null"
+//                    + "Use argument like this: -Xmx=514");
+//        }
+//        for (String str : args) {
+//            if (str.length() == 0 || !str.contains("=")) {
+//                throw new IllegalArgumentException(" Arguments is not correct:"
+//                        + "Use argument like this: -Xmx=514");
+//            }
+//
+//            String[] strings;
+//            strings = str.split("=");
+//            if (strings.length != 2) {
+//                throw new IllegalArgumentException(
+//                        System.lineSeparator()
+//                                + "That form arguments is not correct: -Xmx= OR =514"
+//                                + System.lineSeparator()
+//                                + "Use argument like this: -Xmx=514");
+//            }
+//        }
+//    }
 
     public static void main(String[] args) {
 
