432c4bb1236d6d87b3684bf30daac3d5ebd32a78
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-01 12:19:34 +0300

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,18 @@
+package ru.job4j.question;
+
+import java.util.Set;
+
+/**
+ * https://job4j.ru/profile/exercise/44/task-view/304
+ * Это задание сводится к определению разницы между
+ * начальным и измененным состояниями множества.
+ *
+ * @author AlexSapsay (sapsayalexey@gmail.com)
+ * @version 1.0
+ * @since 31.10.2021
+ */
+public class Analize {
+    public static Info diff(Set<User> previous, Set<User> current) {
+        return null;
+    }
+}
