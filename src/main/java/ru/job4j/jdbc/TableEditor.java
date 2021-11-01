612ee41a461776fb39d66df5aa6ef492ef8afe08
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-01 12:07:18 +0300

    2. Добавить метод boolean isBinary() [#1712] исправляю чекстайл 2

--- src/main/java/ru/job4j/jdbc/TableEditor.java
+++ src/main/java/ru/job4j/jdbc/TableEditor.java
@@ -178,25 +178,25 @@ public class TableEditor implements AutoCloseable {
         System.out.println("--- Таблица созданна ---");
 
         /**
-         * // System.out.println("--- Удаляем таблицу Test_table1---");
-         * // tableEditor.dropTable("Test_table1");
-         * // System.out.println("--- Таблица удаленна---");
+         * System.out.println("--- Удаляем таблицу Test_table1---");
+         * tableEditor.dropTable("Test_table1");
+         * System.out.println("--- Таблица удаленна---");
          *
-         * // System.out.println("--- Создаем тестовую таблицу без полей---");
-         * // tableEditor.createTable("Test_table1");
-         * // System.out.println("--- Таблица созданна ---");
+         * System.out.println("--- Создаем тестовую таблицу без полей---");
+         * tableEditor.createTable("Test_table1");
+         * System.out.println("--- Таблица созданна ---");
          *
-         * // System.out.println("--- Добравляем колонку в таблицу ---");
-         * // tableEditor.addColumn("Test_table1", "adding", "INT");
-         * // System.out.println("--- Колонка добавленна ---");
+         * System.out.println("--- Добравляем колонку в таблицу ---");
+         * tableEditor.addColumn("Test_table1", "adding", "INT");
+         * System.out.println("--- Колонка добавленна ---");
          *
-         * // System.out.println("--- Удаляем колонку  ---");
-         * // tableEditor.dropColumn("Test_table1", "xxx");
-         * // System.out.println("--- Колонка удалена ---");
+         * System.out.println("--- Удаляем колонку  ---");
+         * tableEditor.dropColumn("Test_table1", "xxx");
+         * System.out.println("--- Колонка удалена ---");
          *
-         * // System.out.println("--- Переименновываем колонку  ---");
-         * // tableEditor.renameColumn("Test_table1", "adding1", "XXX");
-         * // System.out.println("--- Колонка переименнованна ---");
+         * System.out.println("--- Переименновываем колонку  ---");
+         * tableEditor.renameColumn("Test_table1", "adding1", "XXX");
+         * System.out.println("--- Колонка переименнованна ---");
          */
     }
 }
