6497bc1a2c6cdaa7b0afa038186d04abafe3d7c7
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-05 20:01:58 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -1,5 +1,7 @@
 package ru.job4j.question;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -13,6 +15,54 @@ import java.util.Set;
  */
 public class Analize {
     public static Info diff(Set<User> previous, Set<User> current) {
-        return null;
+        int added = 0;
+        int changed = 0;
+        int deleted = 0;
+
+        /**
+         *  - Сколько изменено пользователей. Изменённым считается объект,
+         *  в котором изменилось имя, а id осталось прежним.
+         */
+        Map<Integer, String> hashMapPrev = new HashMap();
+        for (User user : previous) {
+            hashMapPrev.put(user.getId(), user.getName());
+        }
+
+        Map<Integer, String> hashMapCurrent = new HashMap();
+        for (User user : current) {
+            hashMapCurrent.put(user.getId(), user.getName());
+        }
+
+        for (User user : current) {
+            if (hashMapPrev.containsKey(user.getId())
+                    && (!user.getName().equals(hashMapPrev.get(user.getId())))) {
+                changed += 1;
+            }
+        }
+
+        /**
+         *  - Сколько добавлено новых пользователей. Добавленным считается такой
+         *  пользователь, что ранее его не было в множестве previous, но в
+         *  множестве current он есть.
+         */
+        for (User user : current) {
+            if (!previous.contains(user)
+                    && !hashMapPrev.containsKey(user.getId())) {
+                added += 1;
+            }
+        }
+
+        /**
+         *  Сколько удалено пользователей. Удаленным считается такой пользователь,
+         *  что ранее он был в множестве previous, но теперь в множестве
+         *  current его нет.
+         */
+        for (User user : previous) {
+            if (!current.contains(user)
+                    && !hashMapCurrent.containsKey(user.getId())) {
+                deleted += 1;
+            }
+        }
+        return new Info(added, changed, deleted);
     }
 }
