1af25a39ec2c1677cfa34a03b2d020cfaa4937f6
AlekseySapsay <pheniksesalex@mail.ru>
2021-11-08 13:42:41 +0300

    2. Статистика по коллекции. [#45889 #225001] осталось пройти два теста

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -19,19 +19,48 @@ public class Analize {
         int changed = 0;
         int deleted = 0;
 
-        /**
-         *  - Сколько изменено пользователей. Изменённым считается объект,
-         *  в котором изменилось имя, а id осталось прежним.
-         */
+
         Map<Integer, String> hashMapPrev = new HashMap();
         for (User user : previous) {
             hashMapPrev.put(user.getId(), user.getName());
         }
 
-        Map<Integer, String> hashMapCurrent = new HashMap();
-        for (User user : current) {
-            hashMapCurrent.put(user.getId(), user.getName());
-        }
+        /**
+         * map текущих пользователей
+         */
+//        Map<Integer, String> hashMapCurrent = new HashMap();
+//        for (User user : current) {
+//            hashMapCurrent.put(user.getId(), user.getName());
+//
+//            if (previous.contains(user.getId())) {
+//                changed += 1;
+//            }
+//        }
+
+        /**
+         * {@code true} if this set did not already contain the specified
+         *  element
+         */
+//        for (User user : previous) {
+//            if (current.add(user)) {
+//                System.out.println("user exist");
+//                current.add(user);
+//            } else {
+//                System.out.println("User not exist and will be added");
+//            }
+//        }
+
+        /**
+         * достаточно создать одну карту, допустим, текущих пользователей,
+         * и добавлять в нее предыдущих, и в зависимости от результата
+         * добавления - производить необходимые проверки и изменять нужные счетчики.
+         */
+//        for (User user : current) {
+//            if (previous.contains(user.getId())
+//            && (!previous.contains(user.getName()))) {
+//                changed += 1;
+//            }
+//        }
 
         for (User user : current) {
             if (hashMapPrev.containsKey(user.getId())
@@ -39,6 +68,17 @@ public class Analize {
                 changed += 1;
             }
         }
+
+        /**
+         *  - Сколько изменено пользователей. Изменённым считается объект,
+         *  в котором изменилось имя, а id осталось прежним.
+         */
+//        for (User user : current) {
+//            if (hashMapPrev.containsKey(user.getId())
+//                    && (!user.getName().equals(hashMapPrev.get(user.getId())))) {
+//                changed += 1;
+//            }
+//        }
 
         /**
          *  - Сколько добавлено новых пользователей. Добавленным считается такой
@@ -52,14 +92,23 @@ public class Analize {
             }
         }
 
+
         /**
          *  Сколько удалено пользователей. Удаленным считается такой пользователь,
          *  что ранее он был в множестве previous, но теперь в множестве
          *  current его нет.
          */
+//        for (User user : previous) {
+//            if (!current.contains(user)
+//                    && !hashMapCurrent.containsKey(user.getId())) {
+//                deleted += 1;
+//            }
+//        }
+
+
         for (User user : previous) {
             if (!current.contains(user)
-                    && !hashMapCurrent.containsKey(user.getId())) {
+                    && (current.contains(user.getId()))) {
                 deleted += 1;
             }
         }
