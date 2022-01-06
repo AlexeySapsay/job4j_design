fc30ef80617fe3aef5619457949427585ce81fdb
AlekseySapsay <pheniksesalex@mail.ru>
2022-01-06 17:59:43 +0300

    3. Шаблонизатор. [#855] добавил тесты

--- src/test/java/ru/job4j/tdd/GeneratorTest.java
+++ src/test/java/ru/job4j/tdd/GeneratorTest.java
@@ -8,7 +8,6 @@ import java.util.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-
 public class GeneratorTest {
     @Ignore
     @Test
@@ -23,5 +22,52 @@ public class GeneratorTest {
         GeneratorImplementation gI = new
                 GeneratorImplementation();
         assertThat(rsl, is(gI.produce(template, map)));
+    }
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void testWhenNotValidTemplate() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        map.put("person", "me");
+
+        int counter = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (!map.containsValue(entry)
+                    && !map.containsKey(entry)) {
+                counter++;
+            }
+        }
+        if (counter != 0) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    @Ignore
+    @Test(expected = IllegalArgumentException.class)
+    public void testWhenNotValidTemplate2() {
+        GeneratorImplementation gI = new
+                GeneratorImplementation();
+        String template = "I am a ${name}";
+        long countChar = template.chars().filter(ch -> ch == '$').count();
+
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Petr Arsentev");
+        map.put("subject", "you");
+        map.put("person", "me");
+
+        int counter = 0;
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            if (!map.containsValue(entry)
+                    && !map.containsKey(entry)) {
+                counter++;
+            }
+        }
+        if (counter != 0 || (countChar != map.size())) {
+            throw new IllegalArgumentException();
+        }
     }
 }
