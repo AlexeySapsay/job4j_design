cf1dba933a410c32233cd022a23348db6680dfff
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-08-30 19:27:33 +0400

    Утверждения с примитивными типами

--- src/test/java/ru/job4j/assertj/BoxTest.java
+++ src/test/java/ru/job4j/assertj/BoxTest.java
@@ -1,8 +1,10 @@
 package ru.job4j.assertj;
 
+import org.assertj.core.data.Percentage;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.withPrecision;
 
 class BoxTest {
     @Test
@@ -13,9 +15,53 @@ class BoxTest {
     }
 
     @Test
-    void whatsThisThenReturnString() {
-        Box box = new Box(0, 10);
-        String name = box.whatsThis();
-        assertThat(name).isEqualTo("Sphere");
+    void IsThisThenReturnTetrahedron() {
+        Box box = new Box(4, 10);
+        String result = box.whatsThis();
+        assertThat(result).isNotNull()
+                .isEqualTo("Tetrahedron");
+    }
+
+    @Test
+    void IsGetAgeThenThenReturn173_20508075688772() {
+        Box box = new Box(4, 10);
+        double result = box.getArea();
+        assertThat(result).isNotZero()
+                .isPositive()
+                .isGreaterThan(173)
+                .isLessThan(174)
+                .isEqualTo(173.20508075688772);
+    }
+
+    @Test
+    void IsGetAgeThenThenReturn173() {
+        Box box = new Box(4, 10);
+        double result = box.getArea();
+        assertThat(result).isEqualTo(173.0d, withPrecision(0.5d))
+                .isCloseTo(173.0d, withPrecision(0.5d))
+                .isCloseTo(173.0d, Percentage.withPercentage(1.0d))
+                .isGreaterThan(173.0d)
+                .isLessThan(175.0d);
+    }
+
+    @Test
+    void IsGetNumberOfVerticesThenReturnInt() {
+        Box box = new Box(4, 10);
+        int result = box.getNumberOfVertices();
+        assertThat(result).isNotZero();
+    }
+
+    @Test
+    void IsExistThenReturnTrue() {
+        Box box = new Box(4, 10);
+        boolean result = box.isExist();
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void IsNotExistThenReturnFalse() {
+        Box box = new Box(0, 0);
+        boolean result = box.isExist();
+        assertThat(result).isFalse();
     }
 }
