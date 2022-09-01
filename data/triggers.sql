bf05514260eceb374a925d8de389058fa45a835f
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-09-01 16:05:12 +0400

    создание и вызов тригеров

--- data/triggers.sql
+++ data/triggers.sql
@@ -38,7 +38,7 @@ $$
     BEGIN
         update products
         set price = price - price * 0.3
-        where id = (select id from inserted) and count >=1;
+        where id = select id from inserted;
         return new;
     END;
 $$
@@ -54,26 +54,40 @@ create trigger tax_trigger
 
 
 --2 Триггер должен срабатывать до вставки данных и высчитывать налог на товар. Здесь используем row уровень.
-
-
-create or replace function tax_before()
+create or replace function tax_before_insert()
     returns trigger as
 $$
     BEGIN
-        update products
-        set price = price - price * 0.13
-        where count >=1 AND id = new.id;
+        new.price = new.price - new.price * 0.13;
         return NEW;
     END;
 $$
 LANGUAGE 'plpgsql';
 
+-- products перед вставкой данных
+--6	product_3	producer_3	8	138
+--8	product_3	producer_3	8	138
+
+-- добавляем новый продукт и автоматически расчитываем налог
+insert into products (name, producer, count, price) values ('product_3', 'producer_3', 8, 115);
+-- налог будет равен 115-115*0,13= 14,95 округаляем в большу сторону и получаем 15
+-- и стоимость продукта 115-15=100, что и видим в таблице ниже
+
+select * from products;
+--теперь products содержит
+--6	product_3	producer_3	8	138
+--8	product_3	producer_3	8	138
+--11	product_3	producer_3	8	100
+
+
+
+
 
 create trigger tax_before_trigger
-after insert
+before insert
 on products
 for each row
-execute procedure tax_before();
+execute procedure tax_before_insert();
 
 
 drop trigger tax_before_trigger on products;
@@ -87,9 +101,10 @@ create table history_of_price (
 );
 
 
+
+
 --3 Нужно написать триггер на row уровне, который при вставке продукта в таблицу products,
 -- будет заносить имя, цену и текущую дату в таблицу history_of_price.
-
 update history_of_price
 set name = 'product_111', price = 11, date = '2222-07-25 10:30:30'
 WHERE price!=0;
@@ -118,8 +133,7 @@ alter table products disable trigger discount_trigger;
 drop trigger copy_trigger_to_history_of_price on products;
 
 select * from products;
-
-SELECT * FROM history_of_price;
+select * from history_of_price;
 
 insert into products (name, producer, count, price) values ('product_3', 'producer_3', 8, 115);
 insert into products (name, producer, count, price) values ('product_1333', 'producer_1', 1111, 51341);
@@ -129,4 +143,4 @@ insert into history_of_price (name, price, date) values ('product_1', 11,  '2018
 
 update history_of_price
 set name = 'product_111', price = 11, date = '2222-07-25 10:30:30'
-WHERE price!=0;
+where price!=0;
