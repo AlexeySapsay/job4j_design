8fd4375985cb5344b376edc76fa15ef51b5c04d6
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-10-06 18:21:15 +0400

    7. Scanner [#504791]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -3,55 +3,57 @@ package ru.job4j.io;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
 public class ArgsName {
+
     private final Map<String, String> values = new HashMap<>();
 
-    /**
-     * Геттер для получения значения value по ключу key
-     *
-     * @param key ключь для извлечения значения value из hashMap
-     * @return value, возвращаемоое значение из хэшмапы
-     */
     public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("The key doesn't exist");
+        }
         return values.get(key);
     }
 
-    /**
-     * Метод получения множества ключей
-     *
-     * @return множество ключей из values
-     */
     public Set<String> getKeys() {
         return values.keySet();
     }
 
-    /**
-     * Метод валидации входных параметров,
-     * заданных через терминал или командную строку
-     *
-     * @param args параметры, полученные из терминала или командной строки
-     */
     private void parse(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Use correct argument like key=value");
-        }
-        for (String str : args) {
-            String[] keyAndValue = str.split("=");
-            if (keyAndValue.length < 2) {
-                throw new IllegalArgumentException("Use like key=value");
-            }
-            String key = keyAndValue[0];
-            String value = keyAndValue[1];
-            values.put(key.substring(1), value);
+        for (String arg : args) {
+            validate(arg);
+            String[] pair = arg.split("=", 2);
+            values.put(pair[0].substring(1), pair[1]);
         }
     }
 
-    public ArgsName of(String[] args) {
+    public static ArgsName of(String[] args) {
         ArgsName names = new ArgsName();
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Arguments are empty");
+        }
         names.parse(args);
         return names;
     }
 
+    public static void validate(String arg) {
+        if (!arg.contains("=")) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must contain the character \"=\"");
+        }
+        if (arg.startsWith("-=") || arg.startsWith("=") || arg.indexOf("=") == arg.length() - 1) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must contain the key");
+        }
+        if (!arg.startsWith("-")) {
+            throw new IllegalArgumentException("The key - value pair " + arg + " must start with the character \"-\"");
+        }
+
+    }
+
     public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
     }
 }
