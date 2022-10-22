eaf5cfe68e6aff77948629511dc4b94f456f8f3f
Сапсай Алексей Юрьевич <ASapsay@alfabank.ru>
2022-10-23 00:15:58 +0400

    0. Принцип подстановки Лисков. [#4915] нарушение предусловия и инвариантности

--- src/main/java/ru/job4j/ood/lsp/strategy/CalculateArea.java
+++ src/main/java/ru/job4j/ood/lsp/strategy/CalculateArea.java
@@ -9,6 +9,9 @@ public class CalculateArea {
     }
 
     public static int calculateSquare(int a, int b) {
+        if (a == 0) {
+            throw new IllegalArgumentException("Argument can't be 0");
+        }
         return a * a;
     }
 }
